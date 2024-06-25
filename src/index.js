db2012a68fa1c1cdaf0f968643eb96ca47158cad
Kamalov <kfarrux2005@gmail.com>
2024-06-25 06:14:02 +0500

    wdth

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import HeadComponent from './compnents/head/Head';
+import CenterComponent from './compnents/center/Center';
 import "./index.css"
 function App() {
   return (
     <div className='container'>
       <HeadComponent />
+      <CenterComponent />
     </div>
   )
 }
