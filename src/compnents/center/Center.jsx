7096cd3d3d645a2050b6997d10e67316a8169124
Kamalov <kfarrux2005@gmail.com>
2024-06-25 06:42:52 +0500

    wdth

--- src/compnents/center/Center.jsx
+++ src/compnents/center/Center.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import './center.css';
 const CenterComponent = () => {
     const coinBalance = 0;
     const [counter, setCounter] = useState(coinBalance);
-    // useEffect('click', ()=>{
-
-    // })
+    const counterFunction = () => {
+        setCounter(counter + 1)
+    }
     return (
         <div className="center">
             <div className="counterCoinBalance">
                 <h2>{coinBalance} $TUC</h2>
             </div>
             <div className="coinImageClicker">
-                <a href="#">
+                <a href="#" onClick={counterFunction}>
                     <img src="https://www.turkicstates.org/assets/vendor/kcfinder/upload/images/turkkon-logo(1).png" alt="coinlogo" />
                 </a>
             </div>
